cf8a9fe46c0cc01c9dbba76ede472611edb22a4d
ADMIN <ADMIN@DESKTOP-JNQOPS2>
2020-06-24 09:12:55 +0700

    Change

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -1,21 +1,12 @@
 <!DOCTYPE html>
 <html>
 <body>
-<head>
-<title>View</title>
-	<link rel="stylesheet" type="text/css" href="Login.css">
-        <link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-</head>
+<h1>DATABASE CONNECTION</h1>
 
 <?php
 ini_set('display_errors', 1);
-
+echo "Database of ATN shop";
 ?>
 
 <?php
@@ -48,9 +39,6 @@ $resultSet = $stmt->fetchAll();
 echo '<p>productinformation:</p>';
 
 ?>
-	
-
-</body>
 <div id="container">
 <table class="table table-bordered table-condensed">
     <thead>
@@ -59,7 +47,7 @@ echo '<p>productinformation:</p>';
         <th>product name</th>
         <th>size</th>
         <th>basicprice</th>
-        <th>residual</th>
+          <th>residual</th>
       </tr>
     </thead>
     <tbody>
